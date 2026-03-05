f43acaa10c78fff621ecb70fbbcde0a8fc102e4f
Jonas Strassel <info@jonas-strassel.de>
2026-03-05 19:25:26 +0100

    Log TRUEBLOCKS_URL on startup

--- src/trueblocks-client.ts
+++ src/trueblocks-client.ts
@@ -3,4 +3,6 @@ import type { paths } from "./trueblocks.d.ts";
 
 const TRUEBLOCKS_URL = process.env.TRUEBLOCKS_URL ?? "http://trueblocks:8080";
 
+console.log(`TrueBlocks URL: ${TRUEBLOCKS_URL}`);
+
 export const trueblocks = createClient<paths>({ baseUrl: TRUEBLOCKS_URL });
