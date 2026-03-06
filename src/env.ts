a15107446a641f231547aacafaf0c2b33bdbc10b
Jonas Strassel <info@jonas-strassel.de>
2026-03-06 17:01:12 +0100

    fix: normalise clickhouse:// scheme to http:// in CLICKHOUSE_URL

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 /** biome-ignore-all lint/style/noProcessEnv: this is the file re-mapping them */
 import arkenv from "arkenv";
 
-export default arkenv({
+const raw = arkenv({
   CLICKHOUSE_URL: "string.url = 'http://localhost:8123'",
   CLICKHOUSE_DB: "string = 'ethereum'",
   HYPERSYNC_API_KEY: "string",
@@ -9,3 +9,8 @@ export default arkenv({
   PORT: "number.port = 3000",
   CHAIN_ID: "number = 1",
 });
+
+export default {
+  ...raw,
+  CLICKHOUSE_URL: raw.CLICKHOUSE_URL.replace(/^clickhouse:\/\//, "http://"),
+};
